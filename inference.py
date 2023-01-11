54ed30ccf0fe72bf2a00c888dc3eec10e7a5b824
Carro <robertmyers@hey.com>
2023-01-10 21:30:15 -0600

    reward model

--- inference.py
+++ inference.py
@@ -6,18 +6,19 @@ import torch
 model = AutoModelForCausalLM.from_pretrained('robertmyers/bpt-sft')
 tokenizer = AutoTokenizer.from_pretrained('EleutherAI/gpt-j-6B')
 world_size = 4
-model = deepspeed.init_inference(model,
+ds_engine = deepspeed.init_inference(model,
                                         mp_size=world_size,
-                                        dtype=torch.float,
+                                        dtype=torch.half,
                                         replace_method='auto',
-                    replace_with_kernel_inject=True)
+                                        replace_with_kernel_inject=True)
 
+model = ds_engine.module
+# input_ids = tokenizer("Hello, my dog is cute", return_tensors="pt")
 
-input_ids = tokenizer("Hello, my dog is cute", return_tensors="pt")
+output = model('Human: what is the capitol of texas?')
 
-output_ids = model(input_ids)
-
-output = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-print(output)
+# output = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
+    print(output)
 # check if on gpu 0 deepspeed, if so, import code; code.interact(local=locals())
-# import code; code.interact(local=locals())
+    import code; code.interact(local=locals())
