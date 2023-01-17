c4d68feed1a4ae5d9224f940c4fd6dd73b11f4f1
Carro <robertmyers@hey.com>
2023-01-17 17:43:52 -0600

    changes

--- reward-modeling/rlhf.py
+++ reward-modeling/rlhf.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     # Load the pre-trained reward model
     rw_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     rw_tokenizer.pad_token = rw_tokenizer.eos_token
-    rw_model = RewardModel(config.model.model_path)
+    rw_model = RewardModel(config.tokenizer.tokenizer_path)
     rw_model.load_state_dict(torch.load(REWARD_CHECKPOINT_PATH))
     rw_model.half()
     rw_model.eval()
