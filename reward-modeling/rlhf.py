5f6ba87736c37ebdd11b3e26589266be37f24be1
Carro <robertmyers@hey.com>
2023-01-17 18:42:54 -0600

    changes

--- reward-modeling/rlhf.py
+++ reward-modeling/rlhf.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     rw_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     rw_tokenizer.pad_token = rw_tokenizer.eos_token
     # rw_model = RewardModel(config.rm.name, rw_tokenizer.eos_token)
-    rw_model = make_rm('Dahoas/gptj-rm-static', "causal", config.tokenizer.tokenizer_path)
+    rw_model = make_rm(config.tokenizer.tokenizer_path, "causal", config.tokenizer.tokenizer_path)
     rw_model.load_state_dict(torch.load(REWARD_CHECKPOINT_PATH))
     rw_model.half()
     rw_model.eval()
