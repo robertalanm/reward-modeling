3f2c01e9c87e0c996a8dace7ff98c5fc74bef227
Carro <robertmyers@hey.com>
2023-01-23 23:46:57 -0600

    forgot some functions

--- reward-modeling/scripts/train_base/run_bpt.sh
+++ reward-modeling/scripts/train_base/run_bpt.sh
@@ -1,3 +1,3 @@
-deepspeed --num_gpus=8 finetune_base.py --config_path ../configs/base_configs/bpt.yaml \
+deepspeed --num_gpus=4 finetune_base.py --config_path ../configs/base_configs/bpt.yaml \
 --ds_config_path ../configs/ds_configs/ds_config_bpt.json \
 --deepspeed ../configs/ds_configs/ds_config_bpt.json
